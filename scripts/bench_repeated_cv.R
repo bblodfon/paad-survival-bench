cd7f4c68a82e18f8929140d8c5cc5fff205eb6f2
john <bblodfon@gmail.com>
2022-06-02 16:30:45 +0200

    remove excessive logging + minimize benchmark result size

--- scripts/bench_repeated_cv.R
+++ scripts/bench_repeated_cv.R
@@ -1,23 +1,10 @@
-######################################################
+#'####################################################
 # Simple Benchmark of 4 untuned survival learners on #
 # the miRNA and mRNA tasks using repeated CV         #
-######################################################
+#'####################################################
 library(mlr3verse)
 library(mlr3proba)
 library(mlr3extralearners)
-library(dplyr)
-library(stringr)
-library(ggplot2)
-
-# Logging ----
-if (FALSE) { # logger doesn't work with parallelization enabled
-  if (!dir.exists('log')) dir.create('log')
-
-  logger = lgr::get_logger("mlr3")
-  logger$set_threshold('info')
-  filename = tempfile(pattern = 'bench_repeated_cv_', tmpdir = 'log', fileext = ".log")
-  logger$add_appender(lgr::AppenderFile$new(filename), name = "logfile")
-}
 
 # Tasks ----
 # first run `scripts/prepare_tasks.R`
@@ -37,9 +24,13 @@ learners = list(glmnet_lrn, rpart_lrn, ranger_lrn, xgboost_lrn)
 resampling = rsmp('repeated_cv', folds = 5, repeats = 10)
 design = benchmark_grid(tasks, learners, resampling)
 
+# Less logging
+lgr::get_logger("bbotk")$set_threshold("warn")
+lgr::get_logger("mlr3")$set_threshold("warn")
+
 # parallelization + reproducibility
 future::plan("multisession")
 set.seed(42)
 
-bm_res = benchmark(design)
-#saveRDS(object = bm_res, file = 'results/bm_res_repeated_cv.rds')
+bm_res = benchmark(design, store_models = TRUE, store_backends = FALSE)
+saveRDS(object = bm_res, file = 'results/bm_res_repeated_cv.rds')
