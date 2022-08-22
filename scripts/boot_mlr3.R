da440f736bf525834039578acd065527b400d9ee
john <bblodfon@gmail.com>
2022-08-22 13:47:36 +0200

    add bootstrap for C-index in separate script

---
+++ scripts/boot_mlr3.R
@@ -0,0 +1,20 @@
+# Bootstrap functions to use with an mlr3 test task and get
+# Confidence Intervals for a performance metric (e.g. C-index)
+
+#' `data` is a data.table/data.frame object with the test data
+#' and has to have the same structure (features and target columns)
+#' as the task that was used to train the provided learner
+boot_fun = function(data, index, learner, measure) {
+  learner$predict_newdata(data[index])$score(measure)
+}
+
+boot_ci = function(data, learner, measure = mlr3::msr('surv.cindex'),
+  num_threads = 1, nrsmps = 1000) {
+
+  boot_res = boot::boot(data, statistic = boot_fun, R = nrsmps,
+    parallel = 'multicore', ncpus = num_threads, learner = learner, measure = measure)
+
+  bootci_res = boot::boot.ci(boot_res, type = c('basic', 'norm', 'perc', 'bca'))
+
+  return(list(boot_res = boot_res, bootci_res = bootci_res))
+}
