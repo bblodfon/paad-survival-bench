96a55a206ee5efafd58f75a42b3899503f2db679
john <bblodfon@gmail.com>
2022-09-01 10:27:01 +0200

    add include_boot_res option
    Saves space when used with FALSE

--- scripts/boot_mlr3.R
+++ scripts/boot_mlr3.R
@@ -9,12 +9,15 @@ boot_fun = function(data, index, learner, measure) {
 }
 
 boot_ci = function(data, learner, measure = mlr3::msr('surv.cindex'),
-  num_threads = 1, nrsmps = 1000, parallel = 'multicore') {
+  num_threads = 1, nrsmps = 1000, parallel = 'multicore', include_boot_res = TRUE) {
 
   boot_res = boot::boot(data, statistic = boot_fun, R = nrsmps,
     parallel = parallel, ncpus = num_threads, learner = learner, measure = measure)
 
   bootci_res = boot::boot.ci(boot_res, type = c('basic', 'norm', 'perc', 'bca'))
 
-  return(list(boot_res = boot_res, bootci_res = bootci_res))
+  if (include_boot_res)
+    return(list(boot_res = boot_res, bootci_res = bootci_res))
+  else
+    return(list(bootci_res = bootci_res))
 }
